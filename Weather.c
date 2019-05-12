e27731b729b76925549b0061bd52c3b7533f460a
patterncard <patcad@o2.pl>
2019-05-12 23:37:08 +0200

    structure, memory, function

--- Weather.c
+++ Weather.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
+
+struct MemoryStruct
+{
+    char *memory;
+    size_t size;
+};
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if (ptr == NULL)
+    {
+        printf("not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
 
 int main()
 {
     CURL *curl = curl_easy_init();
+    CURL *curl_handle = curl_easy_init();
     CURLcode res;
     if (curl)
     {
@@ -18,6 +47,27 @@ int main()
             curl_easy_setopt(curl, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=Bialystok&appid=af84822059c4495720af00937a834639");
             res = curl_easy_perform(curl);
             curl_easy_cleanup(curl);
+
+            struct MemoryStruct chunk;
+            chunk.memory = malloc(1);
+            chunk.size = 0;
+
+            curl_global_init(CURL_GLOBAL_ALL);
+            curl_easy_setopt(curl_handle, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=Bialystok&appid=af84822059c4495720af00937a834639");
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+            res = curl_easy_perform(curl_handle);
+            if (res != CURLE_OK)
+            {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+            }
+            else
+            {
+                printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+            }
+            curl_easy_cleanup(curl_handle);
+            curl_global_cleanup();
             break;
         case 2:
             printf("Gdansk\n");
